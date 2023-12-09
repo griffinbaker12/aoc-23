6fa3b39a8145413b3faad1f75f01b1e675a99104
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-12-09 15:31:11 -0600

    so cool

--- day5/day5.py
+++ day5/day5.py
@@ -37,4 +37,48 @@ def part1():
 
     return min(seeds)
 
-print(part1())
+# print(part1())
+
+# instead of transforming the values, you tranform the range
+def part2():
+    # you want seeds to be a tuple of (s, e)
+    seeds = list(map(int, input[0].split(": ")[1].split()))
+    seed_ranges = []
+
+    for i in range(0, len(seeds), 2):
+        seed_ranges.append((seeds[i], seeds[i] + seeds[i + 1]))
+    
+    blocks = input[1:]
+
+    for block in blocks:
+        ranges = []
+        for line in block.splitlines()[1:]:
+            x = list(map(int, line.split()))
+            ranges.append(x)
+        
+        new = []
+
+        while len(seed_ranges) > 0:
+            s, e = seed_ranges.pop()
+            for a, b, c in ranges:
+                os = max(b, s)
+                oe = min(e, b + c)
+                # if there is an overlap
+                if os < oe:
+                    # apply transformation to the range
+                    new.append((os - b + a, oe - b + a))
+                    # check if other parts of seed range can be applied elsewhere
+                    if s < os:
+                        seed_ranges.append((s, os))
+                    if e > oe:
+                        seed_ranges.append((e, oe))
+                    break
+            else:
+                new.append((s, e))
+
+        # seeds are now equal to their transformed values
+        seed_ranges = new
+    
+    return sorted(seed_ranges)[0][0]
+
+print(part2())
