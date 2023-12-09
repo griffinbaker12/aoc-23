c590f8e3fcc1b10ffe27b63f262160dd8a716cb3
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-12-09 14:22:44 -0600

    cool

--- day5/day5.py
+++ day5/day5.py
@@ -1,10 +1,18 @@
-file_path = 'input.txt'
+file_path = 'test.txt'
 
 location = float('inf')
 
+# the insight is that you only need to store the ranges, and not the values themselves
 with open(file_path, 'r') as file:
     seeds, *blocks = file.read().strip().split('\n\n')
     seeds = list(map(int, seeds.split(": ")[1].split()))
+
+    seed_tuples = []
+
+    for val in range(0, len(seeds), 2):
+        seed_tuples.append((seeds[val], seeds[val] + seeds[val + 1]))
+
+    print(seed_tuples)
 
     for block in blocks:
         # ranges you want to check against
@@ -15,14 +23,22 @@ with open(file_path, 'r') as file:
 
         new = []
 
-        for x in seeds:
+        while len(seed_tuples) > 0:
+            s, e = seed_tuples.pop()
+            print('start', s, 'end', e)
             for a, b, c in ranges:
-                if x in range(b, b + c):
-                    new.append(x - b + a)
+                os = max(s, b)
+                oe = min(e, b + c)
+                if os < oe:
+                    new.append((os + a - b, oe + a - b))
+                    if os > s:
+                        seed_tuples.append((s, os))
+                    if e > oe:
+                        seed_tuples.append((oe, e))
                     break
             else:
-                new.append(x)
+                new.append((s, e))
 
-        seeds = new
-
-    print(min(seeds))
+        seed_tuples = new
+    
+    print(sorted(seed_tuples),  'the tuples are')
